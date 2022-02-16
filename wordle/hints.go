6aee4703fb973e0e2e41d7acc14926750a1014cd
Naveen Mahalingam <jedib0t@outlook.com>
2022-02-16 10:40:42 -0800

    hints: unit-tests for all functions

--- wordle/hints.go
+++ wordle/hints.go
@@ -5,6 +5,11 @@ import (
 	"strings"
 )
 
+const (
+	englishAlphabets              = "abcdefghijklmnopqrstuvwxyz"
+	englishAlphabetsWithoutVowels = "bcdfghjklmnpqrstvwxyz"
+)
+
 var (
 	maxHints = 5
 )
@@ -17,7 +22,7 @@ func generateHints(dictionary []string, attempts []Attempt, alphasStatusMap map[
 	alphasNotPresent := make(map[string]bool)
 	alphasPresent := make(map[string]bool)
 	alphasUnknown := make(map[string]bool)
-	for _, char := range "abcdefghijklmnopqrstuvwxyz" {
+	for _, char := range englishAlphabets {
 		charStr := string(char)
 		switch alphasStatusMap[charStr] {
 		case Unknown:
@@ -34,41 +39,40 @@ func generateHints(dictionary []string, attempts []Attempt, alphasStatusMap map[
 	}
 
 	var words []string
-	// if there are too many unknowns, try words with unknown letters to gather
-	// more data for next round
+	// if there are too many known and unknown letters, try words with unknown
+	// letters to gather more data for next round
 	maxWordLength := calculateMaximumWordLength(dictionary)
-	if len(alphasUnknown) > 20 && len(alphasInCorrectLocation) >= (maxWordLength*75/100) {
-		words = filterWordsWithLettersUnknown(dictionary, alphasUnknown)
+	maxWordLength75Percent := maxWordLength * 75 / 100
+	if len(alphasUnknown) > 20 && len(alphasInCorrectLocation) >= maxWordLength75Percent {
+		words = findWordsWithMostUnknownLetters(dictionary, alphasUnknown)
 	}
 
 	// if it couldn't find any usable words, proceed with normal logic
 	if len(words) == 0 {
-		// build some useful maps
-		correctLocationMap, incorrectLocationMap := buildKnownCharacterLocationMap(attempts)
 		// remove words with letters known to be not present
 		words = filterWordsWithLettersNotPresent(dictionary, alphasNotPresent)
 		// remove words with characters in wrong places
-		words = filterWordsWithLettersInWrongLocations(words, correctLocationMap, incorrectLocationMap)
-		// remove words with characters missing
-		words = filterWordsWithLettersMissing(words, alphasPresent)
+		words = filterWordsWithLettersInWrongLocations(words, attempts)
+		// remove words without letters known to be present
+		words = filterWordsWithoutLetters(words, alphasPresent)
 	}
 
 	// if there is more than one option, try narrowing it down
 	if len(words) > 1 {
-		if len(alphasInCorrectLocation) >= (maxWordLength*75/100) && len(words) >= maxWordLength-1 {
+		if len(alphasInCorrectLocation) >= maxWordLength75Percent && len(words) >= maxHints-1 {
 			// if most letters are in right position, but there are still a lot
 			// more words to choose from, try to make words using the missing
-			// letters (ex.: cra_e; options=crake|crane|crate|crave|craze;
-			// find words with k,n,t,v,z)
+			// letters (ex.: cra_e; options=crake|crane|crape|crave|craze;
+			// find words with k,n,p,t,v,z like knaps|knave)
 			missingLetters := findMissingLetters(words, alphasInCorrectLocation)
 			words = findWordsWithMostMissingLetters(dictionary, missingLetters)
-		} else if len(alphasInCorrectLocation) < (maxWordLength*75/100) && len(words) < 10 {
+		} else if len(alphasInCorrectLocation) < maxWordLength75Percent && len(words) <= maxHints {
 			// if few letters are in right position, and there are only a few
 			// options to choose from, try to make words using the unique
 			// letters in all the words (ex.: _iddy; options=biddy|giddy|kiddy|widdy;
 			// find words with b,g,k,w)
-			missingLetters := findDifferingLetters(words)
-			words = findWordsWithMostMissingLetters(dictionary, missingLetters)
+			differingLetters := findDifferingLetters(words)
+			words = findWordsWithMostMissingLetters(dictionary, differingLetters)
 		} else {
 			// build a frequency map and sort by it to eliminate most common
 			// letters for the next attempt
@@ -104,6 +108,14 @@ func buildCharacterFrequencyMap(words []string) map[string]int {
 
 func buildKnownCharacterLocationMap(attempts []Attempt) (map[string][]int, map[string][]int) {
 	correctLocationMap, incorrectLocationMap := make(map[string][]int), make(map[string][]int)
+	hasValue := func(values []int, val int) bool {
+		for _, v := range values {
+			if val == v {
+				return true
+			}
+		}
+		return false
+	}
 	for _, attempt := range attempts {
 		for idx, char := range attempt.Answer {
 			charStr := string(char)
@@ -111,12 +123,16 @@ func buildKnownCharacterLocationMap(attempts []Attempt) (map[string][]int, map[s
 				if correctLocationMap[charStr] == nil {
 					correctLocationMap[charStr] = make([]int, 0)
 				}
-				correctLocationMap[charStr] = append(correctLocationMap[charStr], idx)
+				if !hasValue(correctLocationMap[charStr], idx) {
+					correctLocationMap[charStr] = append(correctLocationMap[charStr], idx)
+				}
 			} else if attempt.Result[idx] == PresentInWrongLocation {
 				if incorrectLocationMap[charStr] == nil {
 					incorrectLocationMap[charStr] = make([]int, 0)
 				}
-				incorrectLocationMap[charStr] = append(incorrectLocationMap[charStr], idx)
+				if !hasValue(incorrectLocationMap[charStr], idx) {
+					incorrectLocationMap[charStr] = append(incorrectLocationMap[charStr], idx)
+				}
 			}
 		}
 	}
@@ -146,7 +162,18 @@ func calculateMaximumWordLength(words []string) int {
 	return maxWordLength
 }
 
-func filterWordsWithLettersInWrongLocations(words []string, correctLocationMap map[string][]int, incorrectLocationMap map[string][]int) []string {
+func countUniqueLetters(word string) int {
+	uniqueLettersMap := make(map[string]bool)
+	for _, char := range word {
+		uniqueLettersMap[string(char)] = true
+	}
+	return len(uniqueLettersMap)
+}
+
+func filterWordsWithLettersInWrongLocations(words []string, attempts []Attempt) []string {
+	// build some useful maps
+	correctLocationMap, incorrectLocationMap := buildKnownCharacterLocationMap(attempts)
+	// function to parse a word based on the maps
 	hasCharacterInWrongLocation := func(word string) bool {
 		for char, indices := range correctLocationMap {
 			for _, idx := range indices {
@@ -174,25 +201,6 @@ func filterWordsWithLettersInWrongLocations(words []string, correctLocationMap m
 	return rsp
 }
 
-func filterWordsWithLettersMissing(words []string, lettersMap map[string]bool) []string {
-	doesNotHaveAllLetters := func(word string) bool {
-		for char := range lettersMap {
-			if !strings.Contains(word, char) {
-				return true
-			}
-		}
-		return false
-	}
-
-	var rsp []string
-	for _, word := range words {
-		if !doesNotHaveAllLetters(word) {
-			rsp = append(rsp, word)
-		}
-	}
-	return rsp
-}
-
 func filterWordsWithLettersNotPresent(words []string, lettersMap map[string]bool) []string {
 	hasLettersNotPresent := func(word string) bool {
 		for _, char := range word {
@@ -212,19 +220,19 @@ func filterWordsWithLettersNotPresent(words []string, lettersMap map[string]bool
 	return rsp
 }
 
-func filterWordsWithLettersUnknown(words []string, lettersMap map[string]bool) []string {
-	hasAllUnknownLetters := func(word string) bool {
-		for _, char := range word {
-			if !lettersMap[string(char)] {
-				return false
+func filterWordsWithoutLetters(words []string, lettersMap map[string]bool) []string {
+	doesNotHaveAllLetters := func(word string) bool {
+		for char := range lettersMap {
+			if !strings.Contains(word, char) {
+				return true
 			}
 		}
-		return true
+		return false
 	}
 
 	var rsp []string
 	for _, word := range words {
-		if hasAllUnknownLetters(word) {
+		if !doesNotHaveAllLetters(word) {
 			rsp = append(rsp, word)
 		}
 	}
@@ -272,6 +280,7 @@ func findWordsWithMostMissingLetters(words []string, lettersMap map[string]bool)
 		letterSeen := make(map[string]bool)
 		for _, char := range word {
 			charStr := string(char)
+			// count each letter only once per word
 			if !letterSeen[charStr] {
 				if lettersMap[charStr] {
 					score++
@@ -292,4 +301,32 @@ func findWordsWithMostMissingLetters(words []string, lettersMap map[string]bool)
 		return iScore > jScore
 	})
 	return words
+}
+
+func findWordsWithMostUnknownLetters(words []string, lettersMap map[string]bool) []string {
+	hasAllUnknownLetters := func(word string) bool {
+		for _, char := range word {
+			if !lettersMap[string(char)] {
+				return false
+			}
+		}
+		return true
+	}
+
+	var rsp []string
+	for _, word := range words {
+		if hasAllUnknownLetters(word) {
+			rsp = append(rsp, word)
+		}
+	}
+	// sort and move up the words with the maximum number of unique letters
+	sort.SliceStable(rsp, func(i, j int) bool {
+		iCount := countUniqueLetters(rsp[i])
+		jCount := countUniqueLetters(rsp[j])
+		if iCount == jCount { // sort alphabetically
+			return rsp[i] < rsp[j]
+		}
+		return iCount > jCount
+	})
+	return rsp
 }
--- wordle/hints_test.go
+++ wordle/hints_test.go
@@ -1,194 +1,343 @@
 package wordle
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_generateHints_aroma(t *testing.T) {
-	filters := Filters{
+var (
+	testFilters = Filters{
 		WithLength(5),
 	}
-	dictionary := filters.Apply(&wordsEnglish)
-	assert.Contains(t, dictionary, "aroma")
+	testDictionary = testFilters.Apply(&wordsEnglish)
+)
+
+func recordAttempt(alphaStatusMap *map[string]CharacterStatus, attempts *[]Attempt, answer string, result []CharacterStatus) {
+	for idx, charStatus := range result {
+		charStr := string(answer[idx])
+		switch charStatus {
+		case NotPresent:
+			delete(*alphaStatusMap, charStr)
+		case PresentInWrongLocation:
+			if (*alphaStatusMap)[charStr] != PresentInCorrectLocation {
+				(*alphaStatusMap)[charStr] = PresentInWrongLocation
+			}
+		case PresentInCorrectLocation:
+			(*alphaStatusMap)[charStr] = PresentInCorrectLocation
+		}
+	}
+	*attempts = append(*attempts, Attempt{Answer: answer, Result: result})
+}
+
+func Test_generateHints_aroma(t *testing.T) {
+	assert.Contains(t, testDictionary, "aroma")
 	attempts := make([]Attempt, 0)
 	alphaStatusMap := make(map[string]CharacterStatus)
-	for _, r := range "abcdefghijklmnopqrstuvwxyz" {
+	for _, r := range englishAlphabets {
 		alphaStatusMap[string(r)] = Unknown
 	}
 
-	hints := generateHints(dictionary, attempts, alphaStatusMap)
+	hints := generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "arose", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "arose", Result: []CharacterStatus{3, 3, 3, 0, 0}})
-	delete(alphaStatusMap, "s")
-	delete(alphaStatusMap, "e")
-	alphaStatusMap["a"] = PresentInCorrectLocation
-	alphaStatusMap["r"] = PresentInCorrectLocation
-	alphaStatusMap["o"] = PresentInCorrectLocation
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "arose", []CharacterStatus{3, 3, 3, 0, 0})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "bight", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "bight", Result: []CharacterStatus{0, 0, 0, 0, 0}})
-	delete(alphaStatusMap, "b")
-	delete(alphaStatusMap, "i")
-	delete(alphaStatusMap, "g")
-	delete(alphaStatusMap, "h")
-	delete(alphaStatusMap, "t")
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "bight", []CharacterStatus{0, 0, 0, 0, 0})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, 1)
 	assert.Equal(t, "aroma", hints[0])
 }
 
 func Test_generateHints_crave(t *testing.T) {
-	filters := Filters{
-		WithLength(5),
-	}
-	dictionary := filters.Apply(&wordsEnglish)
-	assert.Contains(t, dictionary, "crave")
+	assert.Contains(t, testDictionary, "crave")
 	attempts := make([]Attempt, 0)
 	alphaStatusMap := make(map[string]CharacterStatus)
-	for _, r := range "abcdefghijklmnopqrstuvwxyz" {
+	for _, r := range englishAlphabets {
 		alphaStatusMap[string(r)] = Unknown
 	}
 
-	hints := generateHints(dictionary, attempts, alphaStatusMap)
+	hints := generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "arose", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "arose", Result: []CharacterStatus{2, 3, 0, 0, 3}})
-	delete(alphaStatusMap, "o")
-	delete(alphaStatusMap, "s")
-	alphaStatusMap["a"] = PresentInWrongLocation
-	alphaStatusMap["r"] = PresentInCorrectLocation
-	alphaStatusMap["e"] = PresentInCorrectLocation
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "arose", []CharacterStatus{2, 3, 0, 0, 3})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "crate", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "crate", Result: []CharacterStatus{3, 3, 3, 0, 3}})
-	delete(alphaStatusMap, "t")
-	alphaStatusMap["c"] = PresentInCorrectLocation
-	alphaStatusMap["a"] = PresentInCorrectLocation
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "crate", []CharacterStatus{3, 3, 3, 0, 3})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "knaps", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "knaps", Result: []CharacterStatus{0, 0, 3, 0, 0}})
-	delete(alphaStatusMap, "k")
-	delete(alphaStatusMap, "n")
-	delete(alphaStatusMap, "p")
-	delete(alphaStatusMap, "s")
-	alphaStatusMap["a"] = PresentInCorrectLocation
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "knaps", []CharacterStatus{0, 0, 3, 0, 0})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, 2)
 	assert.Equal(t, "crave", hints[0])
 	assert.Equal(t, "craze", hints[1])
 }
 
 func Test_generateHints_cynic(t *testing.T) {
-	filters := Filters{
-		WithLength(5),
-	}
-	dictionary := filters.Apply(&wordsEnglish)
-	assert.Contains(t, dictionary, "cynic")
+	assert.Contains(t, testDictionary, "cynic")
 	attempts := make([]Attempt, 0)
 	alphaStatusMap := make(map[string]CharacterStatus)
-	for _, r := range "abcdefghijklmnopqrstuvwxyz" {
+	for _, r := range englishAlphabets {
 		alphaStatusMap[string(r)] = Unknown
 	}
 
-	hints := generateHints(dictionary, attempts, alphaStatusMap)
+	hints := generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "arose", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "arose", Result: []CharacterStatus{0, 0, 0, 0, 0}})
-	delete(alphaStatusMap, "a")
-	delete(alphaStatusMap, "r")
-	delete(alphaStatusMap, "o")
-	delete(alphaStatusMap, "s")
-	delete(alphaStatusMap, "e")
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "arose", []CharacterStatus{0, 0, 0, 0, 0})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "unity", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "unity", Result: []CharacterStatus{0, 2, 2, 0, 2}})
-	delete(alphaStatusMap, "u")
-	delete(alphaStatusMap, "t")
-	alphaStatusMap["n"] = PresentInWrongLocation
-	alphaStatusMap["i"] = PresentInWrongLocation
-	alphaStatusMap["y"] = PresentInWrongLocation
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "unity", []CharacterStatus{0, 2, 2, 0, 2})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "calve", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "calve", Result: []CharacterStatus{3, 0, 0, 0, 0}})
-	delete(alphaStatusMap, "a")
-	delete(alphaStatusMap, "l")
-	delete(alphaStatusMap, "v")
-	delete(alphaStatusMap, "e")
-	alphaStatusMap["c"] = PresentInCorrectLocation
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "calve", []CharacterStatus{3, 0, 0, 0, 0})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, 1)
 	assert.Equal(t, "cynic", hints[0])
 }
 
 func Test_generateHints_widdy(t *testing.T) {
-	filters := Filters{
-		WithLength(5),
-	}
-	dictionary := filters.Apply(&wordsEnglish)
-	assert.Contains(t, dictionary, "widdy")
+	assert.Contains(t, testDictionary, "widdy")
 	attempts := make([]Attempt, 0)
 	alphaStatusMap := make(map[string]CharacterStatus)
-	for _, r := range "abcdefghijklmnopqrstuvwxyz" {
+	for _, r := range englishAlphabets {
 		alphaStatusMap[string(r)] = Unknown
 	}
 
-	hints := generateHints(dictionary, attempts, alphaStatusMap)
+	hints := generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "arose", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "arose", Result: []CharacterStatus{0, 0, 0, 0, 0}})
-	delete(alphaStatusMap, "a")
-	delete(alphaStatusMap, "r")
-	delete(alphaStatusMap, "o")
-	delete(alphaStatusMap, "s")
-	delete(alphaStatusMap, "e")
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "arose", []CharacterStatus{0, 0, 0, 0, 0})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "unity", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "unity", Result: []CharacterStatus{0, 0, 2, 0, 3}})
-	delete(alphaStatusMap, "u")
-	delete(alphaStatusMap, "n")
-	delete(alphaStatusMap, "t")
-	alphaStatusMap["i"] = PresentInWrongLocation
-	alphaStatusMap["y"] = PresentInCorrectLocation
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "unity", []CharacterStatus{0, 0, 2, 0, 3})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "dimly", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "dimly", Result: []CharacterStatus{2, 3, 0, 0, 3}})
-	delete(alphaStatusMap, "m")
-	delete(alphaStatusMap, "l")
-	alphaStatusMap["d"] = PresentInWrongLocation
-	alphaStatusMap["i"] = PresentInCorrectLocation
-	alphaStatusMap["y"] = PresentInCorrectLocation
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "dimly", []CharacterStatus{2, 3, 0, 0, 3})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, maxHints)
 	assert.Equal(t, "bewig", hints[0])
 
-	attempts = append(attempts, Attempt{Answer: "bewig", Result: []CharacterStatus{0, 0, 2, 2, 0}})
-	delete(alphaStatusMap, "b")
-	delete(alphaStatusMap, "e")
-	delete(alphaStatusMap, "g")
-	alphaStatusMap["w"] = PresentInWrongLocation
-	alphaStatusMap["i"] = PresentInWrongLocation
-	hints = generateHints(dictionary, attempts, alphaStatusMap)
+	recordAttempt(&alphaStatusMap, &attempts, "bewig", []CharacterStatus{0, 0, 2, 2, 0})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
 	assert.Len(t, hints, 1)
 	assert.Equal(t, "widdy", hints[0])
+}
+
+func Test_generateHints_wists(t *testing.T) {
+	assert.Contains(t, testDictionary, "wists")
+	attempts := make([]Attempt, 0)
+	alphaStatusMap := make(map[string]CharacterStatus)
+	for _, r := range englishAlphabets {
+		alphaStatusMap[string(r)] = Unknown
+	}
+
+	hints := generateHints(testDictionary, attempts, alphaStatusMap)
+	assert.Len(t, hints, maxHints)
+	assert.Equal(t, "arose", hints[0])
+
+	recordAttempt(&alphaStatusMap, &attempts, "arose", []CharacterStatus{0, 0, 0, 2, 0})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
+	assert.Len(t, hints, maxHints)
+	assert.Equal(t, "suint", hints[0])
+
+	recordAttempt(&alphaStatusMap, &attempts, "suint", []CharacterStatus{2, 0, 2, 0, 2})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
+	assert.Len(t, hints, maxHints)
+	assert.Equal(t, "kilts", hints[0])
+
+	recordAttempt(&alphaStatusMap, &attempts, "kilts", []CharacterStatus{0, 3, 0, 3, 3})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
+	assert.Len(t, hints, maxHints)
+	assert.Equal(t, "chimb", hints[0])
+
+	recordAttempt(&alphaStatusMap, &attempts, "chimb", []CharacterStatus{0, 0, 2, 0, 0})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
+	assert.Len(t, hints, maxHints)
+	assert.Equal(t, "aglow", hints[0])
+
+	recordAttempt(&alphaStatusMap, &attempts, "aglow", []CharacterStatus{0, 0, 0, 0, 2})
+	hints = generateHints(testDictionary, attempts, alphaStatusMap)
+	assert.Len(t, hints, 1)
+	assert.Equal(t, "wists", hints[0])
+}
+
+func Test_buildCharacterFrequencyMap(t *testing.T) {
+	freqMap := buildCharacterFrequencyMap([]string{
+		"aroma",
+		"crave",
+		"wists",
+	})
+	assert.Len(t, freqMap, 11)
+	assert.Equal(t, 3, freqMap["a"])
+	assert.Equal(t, 1, freqMap["c"])
+	assert.Equal(t, 1, freqMap["e"])
+	assert.Equal(t, 1, freqMap["i"])
+	assert.Equal(t, 1, freqMap["m"])
+	assert.Equal(t, 1, freqMap["o"])
+	assert.Equal(t, 2, freqMap["r"])
+	assert.Equal(t, 2, freqMap["s"])
+	assert.Equal(t, 1, freqMap["t"])
+	assert.Equal(t, 1, freqMap["v"])
+	assert.Equal(t, 1, freqMap["w"])
+}
+
+func Test_buildKnownCharacterLocationMap(t *testing.T) {
+	correctLocationMap, incorrectLocationMap := buildKnownCharacterLocationMap([]Attempt{
+		{Answer: "arose", Result: []CharacterStatus{0, 0, 0, 0, 0}},
+		{Answer: "unity", Result: []CharacterStatus{0, 0, 2, 0, 3}},
+		{Answer: "dimly", Result: []CharacterStatus{2, 3, 0, 0, 3}},
+	})
+	assert.Len(t, correctLocationMap, 2)
+	assert.Equal(t, []int{4}, correctLocationMap["y"])
+	assert.Equal(t, []int{1}, correctLocationMap["i"])
+	assert.Len(t, incorrectLocationMap, 2)
+	assert.Equal(t, []int{0}, incorrectLocationMap["d"])
+	assert.Equal(t, []int{2}, incorrectLocationMap["i"])
+}
+
+func Test_calculateFrequencyValue(t *testing.T) {
+	freqMap := buildCharacterFrequencyMap([]string{
+		"aroma",
+		"crave",
+		"wists",
+	})
+	assert.Len(t, freqMap, 11)
+
+	assert.Equal(t, 7, calculateFrequencyValue("aroma", freqMap))
+	assert.Equal(t, 8, calculateFrequencyValue("crave", freqMap))
+	assert.Equal(t, 5, calculateFrequencyValue("wists", freqMap))
+	assert.Equal(t, 0, calculateFrequencyValue("zzzzz", freqMap))
+}
+
+func Test_calculateMaximumWordLength(t *testing.T) {
+	maxWordLen := calculateMaximumWordLength([]string{
+		"aroma",
+		"brazen",
+		"redemption",
+		"zzyxx",
+	})
+	assert.Equal(t, 10, maxWordLen)
+}
+
+func Test_countUniqueLetters(t *testing.T) {
+	assert.Equal(t, 4, countUniqueLetters("aroma"))
+	assert.Equal(t, 5, countUniqueLetters("crave"))
+	assert.Equal(t, 4, countUniqueLetters("drama"))
+	assert.Equal(t, 3, countUniqueLetters("momma"))
+}
+
+func Test_filterWordsWithLettersInWrongLocations(t *testing.T) {
+	words := filterWordsWithLettersInWrongLocations(testDictionary, []Attempt{
+		{Answer: "arose", Result: []CharacterStatus{0, 0, 0, 0, 0}},
+		{Answer: "unity", Result: []CharacterStatus{0, 0, 2, 0, 3}},
+		{Answer: "dimly", Result: []CharacterStatus{2, 3, 0, 0, 3}},
+	})
+	assert.True(t, len(words) > 1)
+	for _, word := range words {
+		assert.Equal(t, "i", string(word[1]), word)
+		assert.Equal(t, "y", string(word[4]), word)
+		assert.NotEqual(t, "d", string(word[0]), word)
+		assert.NotEqual(t, "i", string(word[2]), word)
+	}
+}
+
+func Test_filterWordsWithLettersNotPresent(t *testing.T) {
+	words := filterWordsWithLettersNotPresent(testDictionary,
+		map[string]bool{"a": true, "e": true, "i": true, "o": true, "u": true},
+	)
+	assert.True(t, len(words) > 1)
+	for _, word := range words {
+		assert.NotContains(t, word, "a")
+		assert.NotContains(t, word, "e")
+		assert.NotContains(t, word, "i")
+		assert.NotContains(t, word, "o")
+		assert.NotContains(t, word, "u")
+	}
+}
+
+func Test_filterWordsWithoutLetters(t *testing.T) {
+	words := filterWordsWithoutLetters(testDictionary,
+		map[string]bool{"g": true, "m": true, "p": true, "y": true},
+	)
+	assert.True(t, len(words) > 0)
+	for _, word := range words {
+		assert.True(t, strings.Contains(word, "g"), word)
+		assert.True(t, strings.Contains(word, "m"), word)
+		assert.True(t, strings.Contains(word, "p"), word)
+		assert.True(t, strings.Contains(word, "y"), word)
+	}
+}
+
+func Test_findDifferingLetters(t *testing.T) {
+	lettersMap := findDifferingLetters([]string{
+		"biddy",
+		"giddy",
+		"kiddy",
+		"widdy",
+	})
+	assert.Len(t, lettersMap, 4)
+	assert.Contains(t, lettersMap, "b")
+	assert.Contains(t, lettersMap, "g")
+	assert.Contains(t, lettersMap, "k")
+	assert.Contains(t, lettersMap, "w")
+}
+
+func Test_findMissingLetters(t *testing.T) {
+	lettersMap := findMissingLetters(
+		[]string{"crake", "crane", "crape", "crave", "craze"},
+		map[string]bool{"c": true, "r": true, "a": true, "e": true},
+	)
+	assert.Len(t, lettersMap, 5)
+	assert.Contains(t, lettersMap, "k")
+	assert.Contains(t, lettersMap, "n")
+	assert.Contains(t, lettersMap, "p")
+	assert.Contains(t, lettersMap, "v")
+	assert.Contains(t, lettersMap, "z")
+}
+
+func Test_findWordsWithMostMissingLetters(t *testing.T) {
+	words := findWordsWithMostMissingLetters(testDictionary,
+		map[string]bool{"k": true, "n": true, "p": true, "v": true, "z": true},
+	)
+	assert.True(t, len(words) >= 3)
+	assert.Equal(t, "knaps", words[0])
+	assert.Equal(t, "knave", words[1])
+	assert.Equal(t, "knops", words[2])
+}
+
+func Test_findWordsWithMostUnknownLetters(t *testing.T) {
+	alphasUnknown := make(map[string]bool)
+	for _, char := range englishAlphabetsWithoutVowels { // no vowels
+		alphasUnknown[string(char)] = true
+	}
+	words := findWordsWithMostUnknownLetters(testDictionary, alphasUnknown)
+	assert.True(t, len(words) > 1)
+	for _, word := range words {
+		assert.NotContains(t, word, "a")
+		assert.NotContains(t, word, "e")
+		assert.NotContains(t, word, "i")
+		assert.NotContains(t, word, "o")
+		assert.NotContains(t, word, "u")
+	}
 }
