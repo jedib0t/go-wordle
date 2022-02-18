aaa5e70a5922c74f625d6d41a52e835b7d3dd8d9
Naveen Mahalingam <jedib0t@outlook.com>
2022-02-18 11:09:45 -0800

    hide cursor while game is in progress

--- cmd/go-wordle/keyboard.go
+++ cmd/go-wordle/keyboard.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/eiannone/keyboard"
 )
 
 func cleanupKeyboard() {
+	cursorShow()
 	_ = keyboard.Close()
+}
+
+func cursorHide() {
+	fmt.Printf("\x1b[?25l")
+}
+
+func cursorShow() {
+	fmt.Printf("\x1b[?25h")
 }
 
 func initKeyboard() {
 	// over-ride keyboard handling
 	if err := keyboard.Open(); err != nil {
-		//logErrorAndExit(err.Error())
+		logErrorAndExit(err.Error())
 	}
+	cursorHide()
 
 	// ensure cleanupKeyboard gets called on exit
 	exitHandlers = append(exitHandlers, cleanupKeyboard)
